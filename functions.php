1fff09207d33edecd56820857f3f58196e6c180d
gotsanity <gotsanity@gmail.com>
2013-02-25 06:58:47 -0800

    4 column flow for floorboard

--- functions.php
+++ functions.php
@@ -129,8 +129,8 @@ class WeDevs_Bootstrap {
         register_sidebar( array(
             'name' => __( 'floor', 'floor' ),
             'id' => 'floor',
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget' => '</aside>',
+            'before_widget' => '<div class="span3"><div class="floor-area"><aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside></div></div>',
             'before_title' => '<h3 class="widget-title">',
             'after_title' => '</h3>',
         ) );
--- sidebar-floor.php
+++ sidebar-floor.php
@@ -9,11 +9,7 @@
 <div id="floor" class="span12" role="complementary">
     <div class="container">
         <div class="row">
-            <div class="span12">
-			    <div class="floor-area">
-					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('floor') ) : endif; ?>
-        	    </div>
-		    </div>
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('floor') ) : endif; ?>
         </div><!-- .row -->
     </div><!-- .container -->
 </div><!-- #floor .floor-area -->
