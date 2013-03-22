c3f44d3ebae9447d80636462414b5e35c73674fb
gotsanity <gotsanity@gmail.com>
2013-03-22 05:23:33 -0700

    added title styling with custom fonts

--- functions.php
+++ functions.php
@@ -127,6 +127,21 @@ class WeDevs_Bootstrap {
             'after_title' => '</h3>',
         ) );
         register_sidebar( array(
+            'name' => __( 'Sub-header-responsive', 'subheader-responsive' ),
+            'id' => 'subheader-responsive',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ) );        register_sidebar( array(
+            'name' => __( 'Sub-header-responsive', 'subheader-responsive' ),
+            'id' => 'subheader-responsive',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ) );
+        register_sidebar( array(
             'name' => __( 'Sidebar', 'wedevs' ),
             'id' => 'sidebar-1',
             'before_widget' => '<aside id="%1$s" class="widget %2$s">',
